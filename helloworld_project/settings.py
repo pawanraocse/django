27bb323953edcaca188b72f02252b09d5ca9a41c
Pawan <pawan@evolphin.com>
2020-06-06 14:41:17 +0530

    Initail Commit

--- helloworld_project/settings.py
+++ helloworld_project/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'helloworld_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], # new
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
